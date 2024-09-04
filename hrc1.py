64eabcaf0041efc1563b4eb375f7c39555d9a9cb
pedronunes19 <pedromiguelmnunes@gmail.com>
2024-09-04 16:23:30 +0200

    commit

---
+++ hrc1.py
@@ -0,0 +1,39 @@
+import os
+from google.cloud import dialogflow
+
+credential_path = (r'./innate-vigil-434613-v1-3a6d5477e206.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
+
+detect_intent_texts("innate-vigil-434613-v1", "1234", "hey", "en-US")
